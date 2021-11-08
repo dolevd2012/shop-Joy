7011a8d052ca7199554fd63e63346c4e5a64f720
Dolev Damri <74798510+dolevd2012@users.noreply.github.com>
2021-11-08 19:39:41 +0200

    Update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -17,7 +17,7 @@ export default class Products extends Component {
 
 
     componentDidMount() {
-        axios.get('https://myprivateshop.herokuapp.com/products').then((res) => {
+        axios.get('https://myprivateshopjoy.herokuapp.com/products').then((res) => {
             this.setState({
                 webProducts: res.data,
                 count: res.data.length
