2743ac443af9f910b2705b7351d754cfed21daa3
Dolev Damri <74798510+dolevd2012@users.noreply.github.com>
2021-11-09 19:38:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import Product from './components/Products'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 ReactDOM.render(
   <Router>
-    <Route exact path="/" component={Home} />
+    <Route exact path="https://shopandjoy.netlify.app/women" render={() => <Product type="female" /> />
     <Route exact path="https://shopandjoy.netlify.app/women" render={() => <Product type="female" />} />
     <Route exact path="/men" render={() => <Product type="male" />}  />
+    <Route exact path="/women" component={Home} />
   </Router>,
   document.getElementById('root')
 );
