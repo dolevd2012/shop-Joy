3c4792cc522d26cfdb16b9f49ba8ac62b8c05e48
Dolev Damri <74798510+dolevd2012@users.noreply.github.com>
2021-11-09 20:23:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 ReactDOM.render(
   <Router>
     <Route exact path="/" component={Home} />
-    <Route exact path="/women" component={Product} />
+    <Route exact path="/women" render={() => <Product type="woman" />} />
     <Route exact path="/men" render={() => <Product type="male" />} />
   </Router>,
   document.getElementById('root')
