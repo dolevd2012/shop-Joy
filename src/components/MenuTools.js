b97d606e17a020c5b14b1e77b00654ec5d90f36a
Dolev Damri <74798510+dolevd2012@users.noreply.github.com>
2021-11-09 20:27:49 +0200

    Update MenuTools.js

--- src/components/MenuTools.js
+++ src/components/MenuTools.js
@@ -112,10 +112,10 @@ export default class MenuTools extends Component {
                                     <div className="row" style={{ height: '14vw' }}>
                                         <div className="col-11 topCategories" style={{ textAlign: 'center', margin: 'auto' }} >
                                             <ul style={{ margin: 'auto' }}>
-                                                <li><a href="https://shopandjoy.netlify.app/women"><Button>WOMEN</Button></a></li>
-                                                <li><a href="/men"><Button>MEN</Button></a></li>
+                                                <li><Link to="/women"><Button>WOMEN</Button></Link></li>
+                                                <li><Link to="/men"><Button>MEN</Button></Link></li>
                                                 <li><Button>KIDS</Button></li>
-                                                <li><a href="/"><Button>HOME</Button></a></li>
+                                                <li><Link to="/"><Button>HOME</Button></Link></li>
                                             </ul>
                                         </div>
                                         <div className="col-1" style={{ textAlign: 'center', margin: 'auto' }}>
