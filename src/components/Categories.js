1191a15d26e790fc293ce3967bb8b07d68b33d25
Dolev Damri <74798510+dolevd2012@users.noreply.github.com>
2021-11-09 19:54:54 +0200

    Update Categories.js

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 function Categories() {
     return (
@@ -8,6 +9,7 @@ function Categories() {
             <a href="/women"><Button>WOMEN</Button></a>
             <a href="/men"><Button>MEN</Button></a>
             <Button>KIDS</Button>
+            <Link to="/women">women?</Link>
         </div>
 
     )
