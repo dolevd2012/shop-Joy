3046a8058e82a0fc82064e235a25375d5b08c4b8
Dolev Damri <74798510+dolevd2012@users.noreply.github.com>
2021-11-09 15:16:41 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import salesHeaderService from '../service/salesHeaderService'
 import axios from 'axios';
 import MenuTools from './MenuTools'
 
